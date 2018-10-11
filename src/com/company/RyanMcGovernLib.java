9b502e77f6d59a47e74151a2e58bcd9ac28499c4
BT_1N3_15 <rmcgovern8516@bths.edu>
2018-10-11 08:33:45 -0400

    Version 2

--- src/com/company/RyanMcGovernLib.java
+++ src/com/company/RyanMcGovernLib.java
@@ -16,14 +16,25 @@ public class RyanMcGovernLib {
     }
 
     public static void guessAI(){
-        Scanner Answer = new Scanner(System.in);
-        System.out.println("Enter a number between 1 and 100 to be the answer the AI will attempt to guess.");
-        int answer = Answer.nextInt();
-        System.out.println("I will try to guess the number you've inputted!");
+        int min = 1;
+        int max = 100;
+        System.out.println("Think of a number between 1 and 100 to be the answer the AI will attempt to guess.");
+        System.out.println("Type 'higher' if your # is higher than the current guess,'lower' if it's lower, or 'correct' if I got it.");
         int currentGuess = 50;
         Scanner input = new Scanner(System.in);
-        while (!input.equals("correct")){
+        String Input = "";
+        while (!(Input.equals("correct"))){
             System.out.println("My guess is "+currentGuess+". Is that correct?");
+            Input = input.nextLine();
+            if (Input.equals("higher")){
+                min = currentGuess;
+                currentGuess = (max+min)/2;
+            }
+            else if (Input.equals("lower")){
+                max = currentGuess;
+                currentGuess = (max+min)/2;
+            }
         }
+        System.out.println("Congratulations! You won!");
     }
 }
